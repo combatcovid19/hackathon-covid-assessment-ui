8d3c9b823e478d524e8a382ad975d74521926d31
Rajeev Kumar <thakurrajeev37@gmail.com>
2020-07-09 12:07:23 +0530

    added routes for contact tracing

---
+++ src/Components/Pages/ContactTracing/ContactTracing.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ContactTracing extends Component {
+    render() {
+        return (
+            <div>
+                <h3>ContactTracing form</h3>
+            </div>
+        )
+    }
+}
+
+export default ContactTracing;
--- src/Components/Pages/Providers/Providers.js
+++ src/Components/Pages/Providers/Providers.js
@@ -5,6 +5,21 @@ class Providers extends Component {
         return (
             <div>
                 <h3>Provider list</h3>
+                <section>
+                    <strong>Nearby Laboratory</strong>
+                    <address>
+                        <strong>Example Inc.</strong><br/>
+                        1234 Example Street<br/>
+                        India, Example 0987<br/>
+                        <abbr title="Phone">P:</abbr> (123) 456-7890
+                    </address>
+                    <address>
+                        <strong>Example Inc.</strong><br/>
+                        1234 Example Street<br/>
+                        India, Example 0987<br/>
+                        <abbr title="Phone">P:</abbr> (123) 456-7890
+                    </address>
+                </section>
             </div>
         )
     }
